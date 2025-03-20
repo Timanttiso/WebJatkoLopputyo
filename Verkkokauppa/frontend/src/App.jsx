858ea981f4d158e1f547c4814897fb26dea70fc4
Timanttiso <n.asujamaa@gmail.com>
2025-03-20 16:43:52 +0200

    Added shoppingcart page and added the possibility of moving to the shopping cart

--- Verkkokauppa/frontend/src/App.jsx
+++ Verkkokauppa/frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 import './App.css';
 import Header from "./components/Header";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShoppingCart from "./ShoppingCart";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <Header/>
-    </>
+    <Router>
+      
+      <Routes>
+        <Route path="/" element={
+          <Header/>
+          
+          } />
+        <Route path="/shoppingcart" element={
+          <ShoppingCart />
+          
+          } />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ Verkkokauppa/frontend/src/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function ShoppingCart() {
+
+  return (
+    <div>
+      <p>Hello from shopping cart</p>
+    </div>
+  )
+}
+
+export default ShoppingCart;
